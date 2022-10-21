5cb7873ffbd099f3fba9cf3373c0c2616642fde8
John McGovern <johnmcg7@gmail.com>
2022-10-21 16:30:02 -0700

    Single Splunk API Session + Timer

--- Splunk_Downloader.py
+++ Splunk_Downloader.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # coding: utf-8
 
 ## Downloads data from Splunk and writes to an S3 bucket
@@ -7,17 +7,17 @@
 import boto3
 import json
 import pandas as pd
-import pytz
+import time
 
 from io import StringIO
 
 import splunklib.client as spclient
-import splunklib.results as results
 
 
 # AWS: Output Configuration
 s3_bucket = 'splunk-export-to-s3'
 s3_base_key = 'inbox/bot_signal_raw/'
+aws_region_name = 'us-west-2'
 
 
 # Splunk: API Configuration
@@ -30,8 +30,8 @@ splunk_time_format = '%Y-%m-%dT%H:%M:%S.%f'
 # Splunk: Time Range Configuration
 start_time_str = '2022-10-21 00:00'
 start_time_region = 'us/pacific' #'utc'
-range_periods = 54
-range_freq = '1min'  # Data time period for each exported file.
+range_periods = 60  # Number of time periods to generate.
+range_freq = '1min'  # Date/time period length for each exported file.
 use_sampling = False
 
 
@@ -52,9 +52,8 @@ if not use_sampling:
 
 
 # AWS Simple Systems Manager / BOTO Session Setup
-aws = boto3.Session(region_name='us-west-2')
+aws = boto3.Session(region_name=aws_region_name)
 ssm = aws.client('ssm')
-#service = spclient.connect(host=HOST,port=PORT,token=splunk_token)
 s3_resource = aws.resource('s3')
 s3_client = aws.client('s3')
 
@@ -63,23 +62,29 @@ s3_client = aws.client('s3')
 splunk_param = ssm.get_parameter(Name=splunk_api_token_name, WithDecryption=True)
 splunk_token = splunk_param['Parameter']['Value']
 
+# Start time in both specific TZ and UTC
 start_time = pd.Timestamp(start_time_str, tz=start_time_region)
 start_time_utc = start_time.astimezone('utc')
+
+
+# Open Splunk API session (only once)
+service = spclient.connect(host=HOST,port=PORT,token=splunk_token)
+
 
 
 # 
 # Main data export loop
 #
+timer_start = time.time()
 for dt in pd.date_range(start=start_time_utc, periods=range_periods, freq=range_freq):
 
-    service = spclient.connect(host=HOST,port=PORT,token=splunk_token)
-    ts = dt.strftime('%Y%m%d%H%M')
-    
+    # Construct the filename with timestamp, frequency, and sampling ratio.
+    ts = dt.strftime('%Y%m%d%H%M')  # timestamp
     filename = file_name_template.replace('<ts>',ts).replace('<freq>',range_freq)
     if use_sampling:
         filename = filename.replace('<ratio>',str(sample_ratio))
     
-    # Check if file exists in S3
+    # Check if file exists in S3, if yes print message and move on.
     key = s3_base_key + f'{dt.year}/{dt.month:02d}/{dt.day:02d}/'+filename
     result = s3_client.list_objects_v2(Bucket=s3_bucket, Prefix=key)
     if 'Contents' in result:
@@ -87,10 +92,10 @@ for dt in pd.date_range(start=start_time_utc, periods=range_periods, freq=range_
         print(f'{key} exists and is {fsize} bytes.')
         continue
 
-    # Splunk query time 
+    # Splunk earliest/latest query time calulation
     earliest = dt.strftime(splunk_time_format)
     latest = (dt + pd.Timedelta(range_freq) - pd.Timedelta('1ms')).strftime(splunk_time_format)
-    print(dt,ts, key)
+    print("\net/lt:", dt,ts, key)
     
     # Splunk API call
     try:
@@ -101,26 +106,30 @@ for dt in pd.date_range(start=start_time_utc, periods=range_periods, freq=range_
     
     # Parse search results
     raw_list = [json.loads(r) for r in rr.read().decode('utf-8').strip().split('\n')]
-    print("Raw Results:", raw_list)
     search_results = [r['result'] for r in raw_list if r['preview']==False]
 
 
+    # Store the search results in a pandas data frame (2D size-mutable table)
     df = pd.DataFrame(search_results)
+    # "Return a tuple representing the dimensionality of the DataFrame."
     print('results:',df.shape)
 
+    # Initialize empty StringIO object and store dataframe as JSON object in it.
     json_buffer = StringIO()
     df.to_json(json_buffer)
+
+    # Store the StringIO file ojbect in S3.
     print(s3_resource.Object(s3_bucket, key).put(Body=json_buffer.getvalue()))
     
-    # Reset
+
+    # Reset our data structures.
     df = None
     raw_list = None
     json_buffer = None
 
-print('...done')
+timer_end = time.time()
+timer_duration = timer_end - timer_start
 
-
-
-
-
+print('\n\nTime Elapsed (Seconds):', timer_duration)
+print('\n\n== Done ==')
 
